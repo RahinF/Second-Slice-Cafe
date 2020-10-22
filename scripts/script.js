1ae7095271b88c3a98b4bcf3b251b76baf80ccc1
RahinF <s3297352@student.rmit.edu.au>
2020-10-22 12:56:56 +1100

    added comments, menu section improved, fixed contact links structure, call the action button is functional

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,11 @@
-let nav_links = document.getElementById("nav-links").getElementsByTagName('li');
+// Highlight current nav link
+// if the screen falls within the sections area
+// change the link appearance to indicate that it is active
+
+let nav_links = document.getElementById("nav-links").getElementsByTagName("li");
 let sections = document.getElementsByTagName("section");
 
-
-
-
 window.onscroll = function() {
-
-    // if the screen falls within the sections area
-    // change the link appearance to indicate that it is active
 
     for (let index = 0; index < sections.length; index++) {
         let section_top = sections[index].offsetTop;
@@ -23,3 +21,9 @@ window.onscroll = function() {
     }
 
 };
+
+
+// Call to action button scroll to section
+document.getElementById("call-to-action").addEventListener("click", function(){
+    document.getElementById("about-section").scrollIntoView(true);
+});
