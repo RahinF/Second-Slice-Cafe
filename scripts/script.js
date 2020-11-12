6ad13f97ba70355c240b5dab9243023f07c8a36c
RahinF <s3297352@student.rmit.edu.au>
2020-11-12 14:53:52 +1100

    seperated font and colour into their own files, removed unused code, improved css efficiency, fixed section divider pixel misplacement, added overflow feature to hamburger menu, repaced section tag with div (w3 issue when theres no heading), changed about section image tab layout (incomplete), added classes to subsection items

--- scripts/script.js
+++ scripts/script.js
@@ -2,23 +2,27 @@
 // if the screen falls within the sections area
 // change the link appearance to indicate that it is active
 
-let nav_links = document.getElementById("nav-links").getElementsByTagName("li");
-let sections = document.getElementsByTagName("section");
+let nav_links = document.querySelectorAll("nav li");
+let sections = document.querySelectorAll(".section");
+
 
 window.onscroll = function() {
 
-    for (let index = 0; index < sections.length; index++) {
-        let section_top = sections[index].offsetTop;
-        let section_bottom = section_top + sections[index].offsetHeight;
-
+    for (let [index, section] of sections.entries()) {
+        let section_top = section.offsetTop;
+        let section_bottom = section_top + section.offsetHeight;
+        
+        
         if (window.scrollY >= section_top && window.scrollY < section_bottom) {
-            nav_links[index].classList.add("nav-active");
+            nav_links[index].classList.add("active");
         } 
 
         else {
-            nav_links[index].classList.remove("nav-active");
+            nav_links[index].classList.remove("active");
         }
-    }
+
+    };
+
 
 };
 
@@ -35,33 +39,35 @@ document.getElementById("call-to-action").addEventListener("click", function(){
 // Image tabs
 // image and text change when a tab is clicked
 
+let image_tabs =  document.querySelectorAll(".image-tab");
+
 // add action lister to tabs
-document.querySelectorAll(".image-tab").forEach(tab => {
-    tab.addEventListener("click", change_image_tab);
-});
+// document.querySelectorAll(".image-tab").forEach(tab => {
+//     tab.addEventListener("click", change_image_tab);
+// });
 
 
-// set the inital image info to first tab
-let image_text_title = document.getElementById("image-text-title");
-let image_text_info = document.getElementById("image-text-info");
+// // set the inital image info to first tab
+// let image_text_title = document.getElementById("image-text-title");
+// let image_text_info = document.getElementById("image-text-info");
 
-image_text_title.innerText = document.querySelector(".tab-title").innerText;
-image_text_info.innerText = document.querySelector(".tab-info").innerHTML;
+// image_text_title.innerText = document.querySelector(".tab-title").innerText;
+// image_text_info.innerText = document.querySelector(".tab-info").innerHTML;
 
 
-// when clicked changes image src, alt and text.
-function change_image_tab(){
-    let expandImg = document.getElementById("expandedImg");
-    let current_tab_image = this.querySelector(".tab-picture");
+// // when clicked changes image src, alt and text.
+// function change_image_tab(){
+//     let expandImg = document.getElementById("expandedImg");
+//     let current_tab_image = this.querySelector(".tab-picture");
 
-    // change image and alt
-    expandImg.src = current_tab_image.src;
-    expandImg.alt = current_tab_image.alt;
+//     // change image and alt
+//     expandImg.src = current_tab_image.src;
+//     expandImg.alt = current_tab_image.alt;
 
-    // change the text and title
-    image_text_title.innerText = this.querySelector(".tab-title").innerText;
-    image_text_info.innerText = this.querySelector(".tab-info").innerHTML;
-}
+//     // change the text and title
+//     image_text_title.innerText = this.querySelector(".tab-title").innerText;
+//     image_text_info.innerText = this.querySelector(".tab-info").innerHTML;
+// }
 
 
 
@@ -85,7 +91,7 @@ function change_menu_tab() {
 
     // remove the active state from menu tabs
     menu_tabs.forEach(tab => {
-        tab.classList.remove("current-menu-tab"); 
+        tab.classList.remove("active"); 
     });
 
     // remove active state from all menu items
@@ -94,7 +100,7 @@ function change_menu_tab() {
     });
 
     // adds active state to tab and menu items
-    this.classList.add("current-menu-tab");  
+    this.classList.add("active");  
     menu_items[menu_tabs.indexOf(this)].classList.add("active");
 }
 
@@ -106,12 +112,18 @@ function change_menu_tab() {
 // Hamburger Menu
 let hamburger_menu = document.getElementById('hamburger');
 let nav_links_container = document.getElementById('nav-content');
+let body = document.querySelector("body");
 
 
 // toggle hamburger menu
 hamburger_menu.addEventListener("click", function () {
-    nav_links_container.classList.toggle('toggle-nav'); // show links
+    nav_links_container.classList.toggle('toggle'); // show links
     hamburger_menu.classList.toggle('active'); // hamburger animation
+
+    // cant scroll if hamburger is active
+    body.style.overflow = body.style.overflow == "visible" ? "hidden": "visible";
+
+  
 });
 
 
@@ -122,7 +134,7 @@ for (let index = 0; index < nav_links.length; index++) {
     nav_links[index].addEventListener("click", function () {
 
         if(hamburger_menu.classList.contains('active')){
-        nav_links_container.classList.toggle('toggle-nav');
+        nav_links_container.classList.toggle('toggle');
         hamburger_menu.classList.toggle('active');
     }
     });
@@ -132,9 +144,11 @@ for (let index = 0; index < nav_links.length; index++) {
 // removes nav active state if window is resized
 window.addEventListener("resize", function() {
     if (window.innerWidth >= 1366) {
-        nav_links_container.classList.remove("toggle-nav");
-        hamburger_menu.classList.remove('active')};
-  });
+        nav_links_container.classList.remove("toggle");
+        hamburger_menu.classList.remove('active');
+        body.style.overflow = "visible";
+    }
+});
 
 
 
